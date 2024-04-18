220128a1a66f7897a2e1b40f4ac16ae40b7aa491
Kayo <Kayohenriquesilva19@gmail.com>
2024-04-18 10:52:54 -0300

    :sparkles: feat: routes user

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,29 @@
+const { hash } = require("bcryptjs")
+const sqliteConnection = require("../database/sqlite")
+const AppError = require("../utils/AppError")
+
+class UsersController {
+  async create(request, response) {
+    const { name, email, password } = request.body
+    const database = await sqliteConnection()
+    
+    const checkUserExists = await database.get(
+      'SELECT * FROM users WHERE email = (?)', [email],
+    )
+    
+    if(checkUserExists) {
+      throw new AppError('User email already exists', 400)
+    }
+    
+    const hashedPassword = await hash(password, 8)
+
+    await database.run(
+      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      [name, email, hashedPassword]
+    )
+    
+    return response.status(201).json()
+  }
+}
+
+module.exports = UsersController
